ec8ee7e57421f09a50108b1821e12aec7deaa058
AmirHosein <sh.amirhosein@gmail.com>
2024-07-10 01:17:51 +0330

    work on the getServices

--- routes/deltasib.js
+++ routes/deltasib.js
@@ -1,34 +1,53 @@
-import express, { response } from 'express';
-import CryptoJS from 'crypto-js'; // تغییر نام ماژول به CryptoJS برای یکسان بودن
-import axios from 'axios';
+import express, { response } from "express";
+import CryptoJS from "crypto-js"; // تغییر نام ماژول به CryptoJS برای یکسان بودن
+import axios from "axios";
 import FormData from "form-data";
-
+import multer from "multer";
+import https from "https";
 
 const router = express.Router();
+const upload = multer();
+const agent = new https.Agent({rejectUnauthorized: false});
 
-const getToken = () => {
+const getToken = async () => {
     const user = "netxpert";
-    const pass = '12345678aA*';
-    const api_key = CryptoJS.SHA512(pass).toString(CryptoJS.enc.Hex); // اضافه کردن CryptoJS.enc.Hex برای تبدیل به رشته هگزادسیمال
-    
+    const pass = "12345678aA*";
+    const api_key = CryptoJS.SHA512(pass).toString(CryptoJS.enc.Hex);
+  
     const formData = new FormData();
-    formData.append("Action","gettoken");
-    formData.append("Api_Username","netxpert");
-    formData.append("Api_Key",api_key);
+    formData.append("Action", "gettoken");
+    formData.append("Api_Username", "netxpert");
+    formData.append("Api_Key", api_key);
+    try {
+        const response = await axios.post("https://185.126.8.124:1043/1.0/auth/", formData, {
+            headers: formData.getHeaders(),
+            httpsAgent: agent
+        });
+        return response.data.token;
+    } catch (error) {
+        return { error: error.message };
+    }
+};
 
-    axios.post("https://185.126.8.124:1043/1.0/auth/",formData,{
-        headers: formData.getHeaders()
-    })
-    .then(response=>{
-        console.log(response);
-    }).catch(error=>{
-        console.log(error);
-    })
-}
+router.get("/getToken",upload.none(), async (req, res) => {
+    const token = await getToken();
+    res.json(token);
+});
 
-router.get('/getData', (req, res) => {
-    const token = getToken();
-    res.json(token); 
+router.get("/getServices",upload.none(), async (req, res) => {
+    const token = await getToken();
+    try{
+        const response = await axios.get("https://185.126.8.124:1043/1.0/service/?Action=list&Filter=UserChoosable='yes' and ISEnable = 'yes' and ServiceType='Base'",{
+            headers:{
+                Authorization:`Bearer ${token}`
+            },
+            httpsAgent:agent
+        });
+        res.json(response.data);
+    }catch(err){
+        res.json(err);
+    }
+
 });
 
 export default router;
