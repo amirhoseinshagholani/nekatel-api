f9bf7ac122f95cb221744fa27097340af4627a39
amirhoseinshagholani <sh.amirhosein@gmail.com>
2024-07-10 16:27:33 +0330

    working on deltasib

--- routes/deltasib.js
+++ routes/deltasib.js
@@ -7,47 +7,143 @@ import https from "https";
 
 const router = express.Router();
 const upload = multer();
-const agent = new https.Agent({rejectUnauthorized: false});
+const agent = new https.Agent({ rejectUnauthorized: false });
 
-const getToken = async () => {
-    const user = "netxpert";
-    const pass = "12345678aA*";
-    const api_key = CryptoJS.SHA512(pass).toString(CryptoJS.enc.Hex);
-  
-    const formData = new FormData();
-    formData.append("Action", "gettoken");
-    formData.append("Api_Username", "netxpert");
-    formData.append("Api_Key", api_key);
+//function getToken
+const getToken = async (Api_user, Api_pass) => {
     try {
+        const user = Api_user;
+        const pass = Api_pass;
+        const api_key = CryptoJS.SHA512(pass).toString(CryptoJS.enc.Hex);
+
+        const formData = new FormData();
+        formData.append("Action", "gettoken");
+        formData.append("Api_Username", user);
+        formData.append("Api_Key", api_key);
+
         const response = await axios.post("https://185.126.8.124:1043/1.0/auth/", formData, {
             headers: formData.getHeaders(),
             httpsAgent: agent
         });
-        return response.data.token;
+        return response.data;
     } catch (error) {
         return { error: error.message };
     }
 };
 
-router.get("/getToken",upload.none(), async (req, res) => {
-    const token = await getToken();
-    res.json(token);
+//function getUser
+const getUserId = async (user,token)=>{
+    var user;
+    try {
+        const response = await axios.get("https://185.126.8.124:1043/1.0/user/?Action=list", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            httpsAgent: agent
+        });
+        user = response.data.find(res=>{
+            return res.username == user;
+        })
+        return user;
+    } catch (err) {
+        return err;
+    }
+}
+
+router.get("/getToken", upload.none(), async (req, res) => {
+    const Api_user = req.query.Api_User;
+    const Api_pass = req.query.Api_Pass;
+    const auth = await getToken(Api_user, Api_pass);
+    res.json(auth);
 });
 
-router.get("/getServices",upload.none(), async (req, res) => {
-    const token = await getToken();
-    try{
-        const response = await axios.get("https://185.126.8.124:1043/1.0/service/?Action=list&Filter=UserChoosable='yes' and ISEnable = 'yes' and ServiceType='Base'",{
-            headers:{
-                Authorization:`Bearer ${token}`
+router.get("/getServices", upload.none(), async (req, res) => {
+    const Api_user = req.query.Api_User;
+    const Api_pass = req.query.Api_Pass;
+
+    const auth = await getToken(Api_user,Api_pass);
+    try {
+        const response = await axios.get("https://185.126.8.124:1043/1.0/service/?Action=list&Filter=UserChoosable='yes' and ISEnable = 'yes' and ServiceType='Base'", {
+            headers: {
+                Authorization: `Bearer ${auth.token}`
             },
-            httpsAgent:agent
+            httpsAgent: agent
         });
         res.json(response.data);
-    }catch(err){
+    } catch (err) {
         res.json(err);
     }
-
 });
+
+router.get("/getUsers", upload.none(), async (req, res) => {
+    const Api_user = req.query.Api_User;
+    const Api_pass = req.query.Api_Pass;
+
+    const auth = await getToken(Api_user, Api_pass);
+    try {
+        const response = await axios.get("https://185.126.8.124:1043/1.0/user/?Action=list", {
+            headers: {
+                Authorization: `Bearer ${auth.token}`
+            },
+            httpsAgent: agent
+        });
+        res.json(response.data);
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+router.get("/getActiveServiceOfUser", async (req, res) => {
+    const Api_user = req.query.Api_User;
+    const Api_pass = req.query.Api_Pass;
+
+    const auth = await getToken(Api_user, Api_pass);
+    const username = req.query.username;
+
+    try {
+        const response = await axios.get(`https://185.126.8.124:1043/1.0/user/?Action=creditinfo&User_Id=0&Filter=Username = ${username}`, {
+            headers: {
+                Authorization: `Bearer ${auth.token}`
+            },
+            httpsAgent: agent
+        });
+        res.json(response.data);
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+router.get("/getAllServicesOfUser", async (req, res) => {
+    
+    const Api_user = req.query.Api_User;
+    const Api_pass = req.query.Api_Pass;
+    const username = req.query.username;
+
+    const auth = await getToken(Api_user, Api_pass);
+    const user = await getUserId(username,auth.token);
+    // res.json(user.user_id);
+    
+    try {
+        const response = await axios.get(`https://185.126.8.124:1043/1.0/user.service.base/?Action=list&User_Id=${user.user_id}`, {
+            headers: {
+                Authorization: `Bearer ${auth.token}`
+            },
+            httpsAgent: agent
+        });
+        res.json(response.data);
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+router.post("/AddServiceToUser",upload.none(),async(req,res)=>{
+    // {
+    //     "Action": "add",
+    //     "User_Id": "6",
+    //     "Service_Id": "63",
+    //     "PayPlan": "PostPaid"
+    // }
+    res.json(req.body);
+})
 
 export default router;
