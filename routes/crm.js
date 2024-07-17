4e8a64ed311b75bfae0ec854834aa03f6222c2cf
AmirHosein <sh.amirhosein@gmail.com>
2024-07-18 01:10:00 +0330

    fixed crm.js

--- routes/crm.js
+++ routes/crm.js
@@ -5,10 +5,13 @@ import axios from "axios";
 import FormData from "form-data";
 import multer from "multer";
 import fs from 'fs';
+import cors from "cors";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage:storage });
+
+router.use(cors());
 
 const getSessionName = async (username) => {
   try {
@@ -18,37 +21,38 @@ const getSessionName = async (username) => {
         method: "GET",
       }
     );
+
     const token = await response_get_token.json();
     const AUTH_TOKEN = "SilbiFn0g0jZs5Ln";
     const initial_token = token.result.token + AUTH_TOKEN;
     const accessKey = md5(initial_token);
+
     const formData = new FormData();
     formData.append("operation", "login");
     formData.append("username", "birashk@outlook.com");
     formData.append("accessKey", accessKey);
-    const response_get_sessionname = await fetch(
-      "https://neka.crm24.io/webservice.php",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-    const sessionName = await response_get_sessionname.json();
-    console.log(sessionName.result.sessionName);
-    return sessionName.result.sessionName;
+    const data = await axios.post('https://neka.crm24.io/webservice.php',formData,{
+      headers: formData.getHeaders(),
+    }).then((res)=>{
+      return res.data.result.sessionName;
+    }).catch(err=>{
+      console.log(err);
+    });
+
+    return data;
   } catch (error) {
     console.error("Error:", error);
-    throw new Error("Something went wrong");
+    throw new Error("Something went wrong");  
   }
 };
-
+ 
 //query
 router.post("/getData", async (req, res) => {
   
   const username = await req.body.username;
   const sessionName = await getSessionName(username);
   const query = await req.body.query;
-  
+   
   try {
     const response = await fetch(
       `https://neka.crm24.io/webservice.php?operation=query&sessionName=${sessionName}&query=${query};`,
@@ -70,7 +74,7 @@ router.post("/postData", upload.single('file'), async (req, res) => {
         const sessionName = await getSessionName(username);
         const element = await req.body.element;
         const elementType = await req.body.elementType;
-    
+    return;
         const formData = new FormData();
         formData.append("operation", "create");
         formData.append("sessionName", sessionName);
